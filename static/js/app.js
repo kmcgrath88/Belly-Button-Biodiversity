66339c42b4154dbc833f2b7a21c415e124c35498
Kristi McGrath <mcgrathkristi@gmail.com>
2020-09-16 18:39:38 -0400

    Updates

--- static/js/app.js
+++ static/js/app.js
@@ -2,113 +2,129 @@
 // Incoming data is internally referred to as incomingData
 // Appending sample IDs to dropdown
 
-function init(){
-    
-d3.json("samples.json").then((incomingData) => {
-    var dropDown = d3.select('#selDataset');
-    //console.log(incomingData);
-    incomingData.names.forEach((name) => {
-        dropDown.append('option').text(name).property('value', name); 
-    
-        
-    })
-    var firstID = incomingData.names[0];
-    optionChanged(firstID);
+function init() {
 
-    
-});
+    d3.json("data/samples.json").then((incomingData) => {
+        var dropDown = d3.select('#selDataset');
+        //console.log(incomingData);
+        incomingData.names.forEach((name) => {
+            dropDown.append('option').text(name).property('value', name);
+        });
+        // Grabbing first ID.
+        var firstID = incomingData.names[0];
+        // Running function on first ID to create initial dashboard.
+        optionChanged(firstID);
+    });
 };
 
-// all one function even for bonus?
-
+// Function called in html to dynamically change dashboard based on Test Subject ID drop down chosen.
 function optionChanged(values) {
-    d3.json("samples.json").then((incomingData) => {
+
+    // Pulling in data and creating function.
+    d3.json("data/samples.json").then((incomingData) => {
+
+        // Storing samples list of dictionaries to variable.
         var samples = incomingData.samples
         //console.log(samples);
+
+        // Filter to match Subject ID in drop down with samples list of dictionaries in data file.
         var sampleIDFilter = samples.filter(value => value.id == values);
         //console.log(sampleIDFilter);
 
+        // Storing subject ID into variable.
         var subjectID = sampleIDFilter[0].id
         //console.log(subjectID);
+
+        // Storing dictionary of matching subject ID into variable.
         var result = sampleIDFilter[0];
         //console.log(result);
+
+        // Storing OTU IDs to variable.
         var otuIDs = result.otu_ids;
         //console.log(otuIDs);
+
+        // Storing OTU Labels to variable.
         var otuLabels = result.otu_labels;
         //console.log(otulabels);
+
+        // Storing Sample Values to variable.
         var sampleValues = result.sample_values;
         //console.log(sampleValues);    
-    
+
+        // Slicing OTU IDs and mapping them to array for yticks.
         var yticks = otuIDs.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
         //console.log(yticks);
 
-    // Create trace for bar graph.
-    var barTrace = [{
-        x: sampleValues.slice(0,10).reverse(),
-        y: yticks,
-        text: otuLabels.slice(0,10).reverse(),
-        type: "bar",
-        orientation: 'h',
-        marker: {
-            color: 'rgba(18,162,169,.75)',
-    }}];
+        // Bar graph trace.
+        var barTrace = [{
+            x: sampleValues.slice(0, 10).reverse(),
+            y: yticks,
+            text: otuLabels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: 'h',
+            marker: {
+                color: 'rgba(18,162,169,.75)',
+            }
+        }];
 
-    // Define the bar graph layout.
-    var barLayout = {
-      title: `Top 10 OTUs for Subject ID: ${subjectID}`,
-      xaxis: { title: "Sample Values" },
-      yaxis: { 
-        title: "OTUs",
-        showticklabels: true}
-    };
+        // Define the bar graph layout.
+        var barLayout = {
+            title: `Top 10 OTUs for Subject ID: ${subjectID}`,
+            xaxis: { title: "Sample Values" },
+            yaxis: {
+                title: "OTUs",
+                showticklabels: true
+            }
+        };
 
-    Plotly.newPlot("bar", barTrace, barLayout);
+        Plotly.newPlot("bar", barTrace, barLayout);
 
-    // Bubble chart
-    var bubbleTrace = [{
-        x: otuIDs,
-        y: sampleValues,
-        mode: "markers",
-        marker: {
-            size: sampleValues,
-            color: otuIDs,
-            colorscale: 'Viridis',
-            
-        },
-        text: otuLabels,
-    }];
+        // Bubble chart trace.
+        var bubbleTrace = [{
+            x: otuIDs,
+            y: sampleValues,
+            mode: "markers",
+            marker: {
+                size: sampleValues,
+                color: otuIDs,
+                colorscale: 'Viridis',
 
-    Plotly.newPlot("bubble", bubbleTrace);
+            },
+            text: otuLabels,
+        }];
 
-    // Gauge chart --- what data should this be pulling from????
-    var gaugeTrace = [
-        {
-            domain: { x: [0, 1], y: [0, 1] },
-            value: 270,
-            title: { text: "Belly Button Washing Frequency" },
-            type: "indicator",
-            mode: "gauge+number"
-        }
-    ];
+        Plotly.newPlot("bubble", bubbleTrace);
 
-    Plotly.newPlot("gauge", gaugeTrace);
+        // Demographic information
+        var info = d3.select('#sample-metadata');
+        // console.log(info);
+        info.html('');
+        var metadata = incomingData.metadata;
+        console.log(metadata);
 
+        var metaIDFilter = metadata.filter(value => value.id == values);
+        console.log(metaIDFilter[0]);
 
-    // Demographic information
-    var info = d3.select('#sample-metadata');
-    // console.log(info);
-    info.html('');
-    var metadata = incomingData.metadata;
-    console.log(metadata);
+        Object.entries(metaIDFilter[0]).forEach(([key, value]) => {
+            info.append('h6').text(`${key}:${value}`);
+        });
 
-    var metaIDFilter = metadata.filter(value => value.id == values);
-    console.log(metaIDFilter[0]);
+        // Gauge chart --- what data should this be pulling from????
+        var washFreq = metaIDFilter[0].wfreq;
+        var gaugeTrace = [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                value: washFreq,
+                title: { text: "Belly Button Washing Frequency" },
+                type: "indicator",
+                mode: "gauge+number"
+            }
+        ];
 
-    Object.entries(metaIDFilter[0]).forEach(([key,value]) => {
-    info.append('h6').text(`${key}:${value}`);
+        Plotly.newPlot("gauge", gaugeTrace);
+
     });
+};
 
-});
-}
-
+// Initializing dashboard.
 init();
